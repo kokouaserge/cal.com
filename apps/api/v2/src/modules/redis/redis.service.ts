253f9861bbfe6e78af1143c825a2407b14d4e112
serge KOKOUA <kokouaserge3@gmail.com>
2024-12-09 15:54:46 +0100

    redis fix connect

--- apps/api/v2/src/modules/redis/redis.service.ts
+++ apps/api/v2/src/modules/redis/redis.service.ts
@@ -11,8 +11,30 @@ export class RedisService implements OnModuleDestroy {
   constructor(readonly configService: ConfigService<AppConfig>) {
     const dbUrl = configService.get<string>("db.redisUrl", { infer: true });
     if (!dbUrl) throw new Error("Misconfigured Redis, halting.");
+    const parsedURL = new URL(dbUrl);
 
-    this.redis = new Redis(dbUrl);
+    this.redis = new Redis({
+      host: parsedURL.hostname,
+      port: Number(parsedURL.port),
+      username: parsedURL.username,
+      password: decodeURIComponent(parsedURL.password),
+    });
+    this.redis.on("error", (err) => {
+      this.logger.error(`Erreur Redis : ${err.message}`);
+    });
+    this.redis.ping((err, pong) => {
+      if (err) {
+        this.logger.error(`Impossible de se connecter à Redis : ${err.message}`);
+        throw err;
+      }
+
+      if (pong !== "PONG") {
+        this.logger.error("Réponse inattendue de Redis lors du ping");
+        throw new Error("Réponse inattendue de Redis lors du ping");
+      }
+
+      console.log("Redis initialisé");
+    });
   }
 
   async onModuleDestroy() {
